74c4b2a499d7c48daeff556a87a72adbca19f912
Piyush Prajapati <piyushoa2004@gmail.com>
2024-09-29 12:42:38 +0530

    version changed to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.piyushprajpti.pyquote"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
